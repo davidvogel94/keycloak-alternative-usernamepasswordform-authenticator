796ff787086edbe7285c0c409d4a513b284e632d
David Vogel <david.vogel@dyedurham.com>
2022-02-25 12:01:08 +1000

    fix: remove 'Alternative' from execution model requirements; added 'disabled' instead
    fix: update help text to accurately reflect what the authenticator does

--- src/main/java/com/davidvogel94/sandbox/keycloak/AlternativeUsernamePasswordFormFactory.java
+++ src/main/java/com/davidvogel94/sandbox/keycloak/AlternativeUsernamePasswordFormFactory.java
@@ -13,7 +13,7 @@ public class AlternativeUsernamePasswordFormFactory extends UsernamePasswordForm
     public static final String PROVIDER_ID = "alternative-username-password-form";
     public static final AlternativeUsernamePasswordForm SINGLETON = new AlternativeUsernamePasswordForm();
     public static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
-            AuthenticationExecutionModel.Requirement.REQUIRED, AuthenticationExecutionModel.Requirement.ALTERNATIVE
+            AuthenticationExecutionModel.Requirement.REQUIRED, AuthenticationExecutionModel.Requirement.DISABLED
     };
 
     @Override
@@ -38,6 +38,6 @@ public class AlternativeUsernamePasswordFormFactory extends UsernamePasswordForm
 
     @Override
     public String getHelpText() {
-        return "Validates a username and password from login form. Accepts when marked as 'alternative' to continue the execution flow if the validation fails but stops all when it's marked as 'Required'.";
+        return "Validates a username and password from login form. Informs the user that their account is disabled (e.g. from permanent lockout due to brute force attack) if and only if they supply the correct credentials.";
     }
 }
